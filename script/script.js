ff353db321e68302ebd1fa1bad9b49b274ec42da
Shakir ALi <shakirbugti926@gmail.com>
2022-05-07 15:46:50 +0500

    Url block works manually by now

--- script/script.js
+++ script/script.js
@@ -1,57 +1,166 @@
-window.addEventListener(
-  "DOMContentLoaded",
-  (event) => {
-    console.log("ran");
-    const images = [
-      "picture (1).jpg",
-      "picture (2).jpg",
-      "picture (3).jpg",
-      "picture (4).jpg",
-      "picture (5).jpg",
-    ];
-    fetch("https://source.unsplash.com/1600x900/?nature").then((response) => {
-      let image = response.url;
-      console.log(image);
-      document.getElementById("image").setAttribute("src", `${image}`);
-    });
-    // const image = images[Math.floor(Math.random() * images.length)];
+const notFoundPage404 = (domainName) => {
+  return `<div class="mars"></div>
+    <img src="https://assets.codepen.io/1538474/404.svg" class="logo-404" />
+    <img src="https://assets.codepen.io/1538474/meteor.svg" class="meteor" />
+    <p class="title">Oh no!!</p>
+    <p class="subtitle">
+      You’re either not allowed to Visit <span style="color:white; font-family: fantasy; letter-spacing: 2px;">${domainName.toUpperCase()}</span> <br />
+      or requesting a page that's no longer here.
+    </p>
+    <div align="center">
+      <a class="btn-back" href="#">Back to previous page</a>
+    </div>
+    <img
+      src="https://assets.codepen.io/1538474/astronaut.svg"
+      class="astronaut"
+    />
+    <img
+      src="https://assets.codepen.io/1538474/spaceship.svg"
+      class="spaceship"
+    />`;
+};
 
-    var todayTarget = document.getElementById("todayTarget");
-    var toDoList = document.getElementById("to-do-List");
+const generateStyling = () => {
+  return `
+  <style>
+  @import url("https://fonts.googleapis.com/css?family=Nunito:400,600,700");
+  @keyframes floating {
+    from {
+      transform: translateY(0px);
+    }
+    65% {
+      transform: translateY(15px);
+    }
+    to {
+      transform: translateY(0px);
+    }
+  }
+  html {
+    height: 100%;
+  }
+  body {
+    background-image: url("https://assets.codepen.io/1538474/star.svg"),
+      linear-gradient(to bottom, #05007a, #4d007d);
+    height: 100%;
+    margin: 0;
+    background-attachment: fixed;
+    overflow: hidden;
+  }
+  .mars {
+    left: 0;
+    right: 0;
+    bottom: 0;
+    position: absolute;
+    height: 27vmin;
+    background: url("https://assets.codepen.io/1538474/mars.svg") no-repeat bottom
+      center;
+    background-size: cover;
+  }
+  .logo-404 {
+    position: absolute;
+    margin-left: auto;
+    margin-right: auto;
+    left: 0;
+    right: 0;
+    top: 16vmin;
+    width: 30vmin;
+  }
+  @media (max-width: 480px) and (min-width: 320px) {
+    .logo-404 {
+      top: 45vmin;
+    }
+  }
+  .meteor {
+    position: absolute;
+    right: 2vmin;
+    top: 16vmin;
+  }
+  .title {
+    color: white;
+    font-family: "Nunito", sans-serif;
+    font-weight: 600;
+    text-align: center;
+    font-size: 5vmin;
+    margin-top: 31vmin;
+  }
+  @media (max-width: 480px) and (min-width: 320px) {
+    .title {
+      margin-top: 65vmin;
+    }
+  }
+  .subtitle {
+    color: white;
+    font-family: "Nunito", sans-serif;
+    font-weight: 400;
+    text-align: center;
+    font-size: 3.5vmin;
+    margin-top: -1vmin;
+    margin-bottom: 9vmin;
+  }
+  .btn-back {
+    border: 1px solid white;
+    color: white;
+    height: 5vmin;
+    padding: 12px;
+    font-family: "Nunito", sans-serif;
+    text-decoration: none;
+    border-radius: 5px;
+  }
+  .btn-back:hover {
+    background: white;
+    color: #4d007d;
+  }
+  @media (max-width: 480px) and (min-width: 320px) {
+    .btn-back {
+      font-size: 3.5vmin;
+    }
+  }
+  .astronaut {
+    position: absolute;
+    top: 18vmin;
+    left: 10vmin;
+    height: 30vmin;
+    animation: floating 3s infinite ease-in-out;
+  }
+  @media (max-width: 480px) and (min-width: 320px) {
+    .astronaut {
+      top: 2vmin;
+    }
+  }
+  .spaceship {
+    position: absolute;
+    bottom: 15vmin;
+    right: 24vmin;
+  }
+  @media (max-width: 480px) and (min-width: 320px) {
+    .spaceship {
+      width: 45vmin;
+      bottom: 18vmin;
+    }
+  }
+  </style>
+  `;
+};
 
-    var quote = document.getElementById("quote");
-    var author = document.getElementById("author");
-    fetch("https://type.fit/api/quotes")
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data[Math.floor(Math.random() * data.length)]);
-        let randomQuote = data[Math.floor(Math.random() * data.length)].text;
-        let authorName = data[Math.floor(Math.random() * data.length)].author;
+const blockedSite = ["twitter", "facebook", "google", "instagram", "youtube"];
+let fullHostname = window.location.hostname;
+var splitStr = fullHostname.split(".").length - 1; // subtracting 1 is important!
 
-        quote.innerHTML = `" ${randomQuote} "`;
-        author.innerHTML = authorName;
-      });
+if (splitStr == 2) {
+  let domainName = fullHostname.slice(
+    fullHostname.indexOf(".") + 1,
+    fullHostname.lastIndexOf(".")
+  );
 
-    var focusInput = document.getElementById("focus");
-    chrome.storage.sync.get(["focus"], function (result) {
-      if (result.focus) {
-        todayTarget.innerHTML = result.focus;
-        toDoList.style.visibility = "visible";
-      }
-    });
-
-    focusInput.addEventListener("keypress", function (e) {
-      chrome.storage.sync.set({ focus: e.target.value });
-      let result = e.target.value;
-      if (e.key === "Enter") {
-        todayTarget.innerHTML = result;
-        toDoList.style.visibility = "visible";
-        // // console.log("pressed Enter Key");
-        // console.log(result);
-      }
-    });
-  },
-  false
-);
+  if (blockedSite.includes(domainName)) {
+    document.head.innerHTML = generateStyling();
+    document.body.innerHTML = notFoundPage404(domainName);
+  }
+}
+if (splitStr == 1) {
+  let domainName = fullHostname.slice("", fullHostname.lastIndexOf("."));
+  if (blockedSite.includes(domainName)) {
+    document.head.innerHTML = generateStyling();
+    document.body.innerHTML = notFoundPage404(domainName);
+  }
+}
